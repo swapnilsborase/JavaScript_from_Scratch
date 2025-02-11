0031f64002a5c7fdb750be0a13d28fda65e50ae7
Swapnil Borase <73363115+swapnilborase@users.noreply.github.com>
2025-02-11 09:52:12 +0000

    JavaScript Tutorial

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Learn JavaScript from scratch
 
 ```MDN Documents : https://developer.mozilla.org/en-US/```
+```Patterns : https://medium.com/@HelloMoto69/patterns-in-javascript-1992ae52a26c```
+
--- tut06.js
+++ tut06.js
@@ -1,1 +1,16 @@
-//Tutorial 06 - 
+//Tutorial 06 - Loops in JavaScript
+
+for (let i = 1; i<=5; i++){
+    console.log("Swapnil");
+}
+
+let sum = 0;
+let tot = 100
+
+for (i=0; i<=tot; i++){
+    sum += i;
+    // console.log('sum: ',sum);
+}
+console.log("Sum of first 10 natural numbers is: " + sum);
+console.log('loop ended');
+
